20ccf7a0d0017ac3912ab5a67e3e00d0ab305f81
Мирас Каражигитов <karazhigitov@kolesa.kz>
2019-11-19 17:10:10 +0600

    implemented atomic wrapper

--- Sources/AtomicBomb/AtomicBomb.swift
+++ Sources/AtomicBomb/AtomicBomb.swift
@@ -1,3 +1,33 @@
-struct AtomicBomb {
-    var text = "Hello, World!"
+#if !os(macOS)
+import UIKit
+
+@propertyWrapper
+class Atomic<Value> {
+    private let queue = DispatchQueue(label: "com.miraskarazhigitov.atomic")
+    private var value: Value
+
+    init(wrappedValue: Value) {
+        self.value = wrappedValue
+    }
+    
+    var wrappedValue: Value {
+        get {
+            return queue.sync { value }
+        }
+        set {
+            queue.sync { value = newValue }
+        }
+    }
+    
+    var projectedValue: Atomic<Value> {
+        return self
+    }
+    
+    func mutate(_ mutation: (inout Value) -> Void) {
+        return queue.sync {
+            mutation(&value)
+        }
+    }
 }
+
+#endif
--- Tests/AtomicBombTests/AtomicBombTests.swift
+++ Tests/AtomicBombTests/AtomicBombTests.swift
@@ -6,7 +6,7 @@ final class AtomicBombTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AtomicBomb().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 
     static var allTests = [
